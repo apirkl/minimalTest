8ffb1f31128cdedcf8a83f5b098e89c0129029f2
andrew.pirkl <andrew.pirkl@jamf.com>
2024-08-30 15:19:46 -0500

    remove annotations I forgot about

--- src/main/kotlin/com/minimalTest/GithubController.kt
+++ src/main/kotlin/com/minimalTest/GithubController.kt
@@ -1,6 +1,5 @@
 package com.minimalTest
 
-import io.micronaut.core.async.annotation.SingleResult
 import io.micronaut.core.io.buffer.ByteBuffer
 import io.micronaut.http.HttpHeaders.USER_AGENT
 import io.micronaut.http.HttpRequest
@@ -11,20 +10,16 @@ import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.ProxyHttpClient
 import io.micronaut.http.client.StreamingHttpClient
 import io.micronaut.http.uri.UriBuilder
-import io.micronaut.scheduling.TaskExecutors
-import io.micronaut.scheduling.annotation.ExecuteOn
 import org.reactivestreams.Publisher
 import java.net.URI
 
 @Controller
-@ExecuteOn(TaskExecutors.BLOCKING) // make use of netty parallelization and virtual threading
 class GithubController(
     private val proxyHttpClient: ProxyHttpClient,
     private val httpClient: HttpClient,
     private val streamingHttpClient: StreamingHttpClient
     ) {
     @Get("/proxy")
-    @SingleResult
     fun proxiedClient(): Publisher<MutableHttpResponse<*>>? {
         val uri = UriBuilder.of(URI("https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting")).build()
         val req = HttpRequest.GET<Any>(uri)
